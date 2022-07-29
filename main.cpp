a41d68eacf4802bacdd14f93faf909395d7ba04e
HaiJaine <2215654043@qq.com>
2022-07-29 22:19:25 +0800

    初始化

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 int main() {
     int x = 10;
     std::cout << x;
-    std::cout << "你好！" << std::endl;
+    std::cout << "你好！！！" << std::endl;
     std::cout << "hello" << std::endl;
     return 0;
 }
